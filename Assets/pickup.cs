b5e1c5a0167a5725cc91e826a8e626c32fe391b0
Tyler Cauthen <>
2019-06-16 12:59:47 -0400

    pickup animation

---
+++ Assets/pickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour
+{
+    Animator animController;
+    public DynamicBone spine, l_arm, r_arm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animController = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            l_arm.enabled = false;
+            r_arm.enabled = false;
+            spine.enabled = false;
+            spine.m_Damping = 0.229f;
+            spine.m_Stiffness = 0.869f;
+            animController.SetInteger("action", 1);
+
+
+        }
+        else
+        {
+            //animController.SetInteger("action", 0);
+            //l_arm.enabled = true;
+            //r_arm.enabled = true;
+        }
+    }
+}
