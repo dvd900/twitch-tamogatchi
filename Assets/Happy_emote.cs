cfedb7bc4cfd0f02c3f95fbc726573e024798f64
Tyler Cauthen <>
2020-03-27 13:15:45 -0400

    reconfigured arm bones for better use with IK. Cheer animation. bomb

---
+++ Assets/Happy_emote.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Happy_emote : MonoBehaviour
+{
+    public Renderer Eye1, Eye2;
+    public Material Default, Happy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MakeHappy()
+    {
+        Eye1.material = Happy;
+        Eye2.material = Happy;
+    }
+    public void MakeDefault()
+    {
+        Eye1.material = Default;
+        Eye2.material = Default;
+    }
+}
---
+++ Assets/moveUpper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveUpper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x + 0.0002f, gameObject.transform.localPosition.y + 0.0002f, gameObject.transform.localPosition.z + 0.0002f);
+    }
+}
