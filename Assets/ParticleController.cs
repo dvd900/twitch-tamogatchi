add143e8f5fed4eb7184e5639e97fd57f7b2ff63
Tyler Cauthen <>
2020-04-11 22:23:02 -0400

    vfx/ explosion

---
+++ Assets/ParticleController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public Animator ac_crumbs;
+    public ParticleSystem Crumbs, psSurprise, psStars, psSparkles;
+    public GameObject AnimeCrumbs;
+    void Start()
+    {
+        psSparkles.Stop();
+        Crumbs.GetComponent<ParticleSystem>().emissionRate = 0;
+        AnimeCrumbs.SetActive(false);
+    }
+
+    void Update()
+    {
+    }
+    public void Crunch()
+    {
+        Crumbs.Emit(3);
+        AnimeCrumbs.SetActive(true);
+        ac_crumbs.Play("animeCrumbs", 0, 0.23f);
+
+    }
+    public void Surprise()
+    {
+        psSurprise.Emit(5);
+        psStars.Emit(3);
+    }
+    public void Sparkles()
+    {
+        psSparkles.Play();
+    }
+    public void SparklesStop()
+    {
+        psSparkles.Stop();
+    }
+    public void EndEat()
+    {
+        AnimeCrumbs.SetActive(false);
+    }
+}
---
+++ Assets/bomb.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bomb : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    public GameObject bomba;
+    // Start is called before the first frame update
+    void Start()
+    {
+        explosion.Stop(true);
+        StartCoroutine(UnExplode(1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator UnExplode(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        bomba.transform.localScale = new Vector3(0, 0, 0);
+        explosion.Stop(true);
+        bomba.SetActive(true);
+        iTween.ScaleTo(bomba, new Vector3(1f, 1f, 1f), 1);
+        StartCoroutine(WaitToExplode(3f));
+    }
+    private IEnumerator WaitToExplode(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        explosion.time = 0;
+        explosion.Play();
+        bomba.SetActive(false);
+        StartCoroutine(UnExplode(1f));
+    }
+}
