ba07b227dab1ee317a02db8fa0220c6e0a0d23b5
Emmanuel Momot <emmanuel@archiemd.com>
2020-09-09 18:57:26 -0400

    look at camera when eating

--- Assets/Scripts/Actions/EatAction.cs
+++ Assets/Scripts/Actions/EatAction.cs
@@ -3,33 +3,51 @@ using System.Collections;
 
 public class EatAction : AIAction {
 
-    public EatAction(Skin skin) : base(skin) {
+    private bool _hasEaten;
+
+    public EatAction(Skin skin) : base(skin)
+    {
 
     }
 
-    public override AIAction Generate() {
+    public override AIAction Generate()
+    {
         return new EatAction(_skin);
     }
 
-    public override void Interrupt() {
+    public override void Interrupt()
+    {
 
     }
 
-    public override bool IsFinished() {
-        return !_skin.itemController.IsEating;
+    public override bool IsFinished()
+    {
+        return !_skin.itemController.IsEating && !_skin.movementController.IsTurning;
     }
 
-    public override void StartAction() {
-        _skin.itemController.EatHeldItem();
+    public override void StartAction()
+    {
+        _skin.movementController.StopWalking();
+        _skin.movementController.FaceCamera();
     }
 
-    public override void UpdateAction() {
+    public override void UpdateAction()
+    {
+        if (!_hasEaten && !_skin.movementController.IsTurning)
+        {
+            _skin.itemController.EatHeldItem();
+            _hasEaten = true;
+        }
     }
 
-    public override float Score(AIWorldData data) {
-        if(_skin.itemController.HeldItem != null && _skin.itemController.HeldItem is Consumable) {
+    public override float Score(AIWorldData data)
+    {
+        if (_skin.itemController.HeldItem != null && _skin.itemController.HeldItem is Consumable)
+        {
             return .55f;
-        } else {
+        }
+        else
+        {
             return 0.0f;
         }
     }
