1c9a0a04b1912f21223ed3986f9a78a476601c57
Tyler Cauthen <>
2020-09-11 10:52:03 -0400

    Trash scanner wip

--- Assets/Scripts/Misc/RoombaLight.cs
+++ Assets/Scripts/Misc/RoombaLight.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class RoombaLight : MonoBehaviour
 {
-    float rotateValue = 0;
+    float rotateValue = 90;
+    double fillValue = 0.75;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,9 @@ public class RoombaLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        gameObject.transform.rotation = Quaternion.Euler(0, rotateValue, 0);
         gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Rotation",rotateValue);
+        gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Fillpercentage", (float)fillValue);
         if (rotateValue >=360)
         {
             rotateValue = 0;
@@ -28,16 +31,17 @@ public class RoombaLight : MonoBehaviour
         iTween.ValueTo(gameObject, iTween.Hash(
             "from", rotateValue,
             "to", 360,
-            "time", 3f,
+            "time", 1.5f,
             "onupdatetarget", gameObject,
             "onupdate", "tweenOnUpdateCallBack",
-            "easetype", iTween.EaseType.easeInOutQuart
+            "easetype", iTween.EaseType.easeInOutQuad
             )
         );
     }
     void tweenOnUpdateCallBack(int newValue)
     {
         rotateValue = newValue;
+
     }
 
 }
