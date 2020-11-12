6ace7b1aeab17db976a8cd9f6524845918b81912
Tyler Cauthen <>
2020-11-12 16:58:28 -0500

    water drone

---
+++ Assets/Prefabs/Items/Interactables/ItemDependencies_Interactables/Water_Drone/WaterDroneTester.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDroneTester : MonoBehaviour
+{
+    public Rigidbody CapRB;
+    public Rigidbody BottleRB;
+    public ParticleSystem WaterStream;
+
+    public float thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WaterStream.enableEmission = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UnCap()
+    {
+        CapRB.isKinematic = false;
+        CapRB.gameObject.transform.parent = null;
+        CapRB.AddForce(Random.RandomRange(-10,10), thrust, Random.RandomRange(-10, 10), ForceMode.Impulse);
+        CapRB.AddTorque(100, 100, 100, ForceMode.Impulse);
+        WaterStream.enableEmission = true;
+
+    }
+    public void DropBottle()
+    {
+        BottleRB.isKinematic = false;
+        BottleRB.gameObject.transform.parent = null;
+        //CapRB.AddForce(Random.RandomRange(-10, 10), thrust, Random.RandomRange(-10, 10), ForceMode.Impulse);
+        WaterStream.enableEmission = false;
+    }
+}
